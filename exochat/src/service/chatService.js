ce23ec3a2b5f6f8a9507b4fd89f344a073946e2e
Francesco <francescobaeli14@gmail.com>
2023-11-16 10:05:51 +0100

    commit stile

---
+++ exochat/src/service/chatService.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+import { LISTA_CHAT_UTENTE, LISTA_CHAT_UTENTE_ID } from "../utility/EndPoint";
+
+const hostName = window.location.hostname;
+
+//LISTACHATUTENTE
+export function findAllChatForUtente(utente, setListaChat){
+    return axios.post(LISTA_CHAT_UTENTE_ID(hostName), utente).then((response)=> {
+        setListaChat(response.data)
+        console.log(response)
+    }).catch(error => {
+        console.error('Errore durante il login:', error);
+               
+      });
+}
